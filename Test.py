387bd728ea9800017e8c1d072d4b1a56b1748ce5
suveck <suveck@gmail.com>
2014-09-25 17:27:27 -0600

    Simmooon, you need to test the Test.py with python3

---
+++ Test.py
@@ -0,0 +1,10 @@
+Simon = 'the mans'
+
+while Simon:
+	print('It was a trap\n you will never escape unless you answer this question correctly...')
+	response = input('What is le Simon...?')
+	if response == Simon:
+		print ('You clever bastard...')
+	else:
+		while True:
+			print ('BUAWHAHA')
